d7a5f7e862414239e10b810c725fa14549af4ab7
idafi <none>
2017-10-12 17:10:25 -0500

    build.bat copies DLL dependenices

--- build.bat
+++ build.bat
@@ -10,6 +10,20 @@ CALL build_noahbot.bat
 IF ERRORLEVEL 1 GOTO fail
 
 ECHO.
+ECHO ----------------------
+ECHO copying dependencies
+ECHO ----------------------
+ECHO.
+
+XCOPY /Q /Y %NOAHBOT_DSHARP_DLL% %NOAHBOT_OUT%
+IF ERRORLEVEL 1 GOTO fail
+
+XCOPY /Q /Y %NOAHBOT_JSONNET_DLL% %NOAHBOT_OUT%
+IF ERRORLEVEL 1 GOTO fail
+
+ECHO done
+
+ECHO.
 ECHO build successful!
 GOTO :EOF
 
