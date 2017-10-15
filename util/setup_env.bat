e660c1df85d56f5c9feeffef06feb3b1f52dedd8
idafi <none>
2017-10-15 14:34:36 -0500

    upgrade csc

--- util/setup_env.bat
+++ util/setup_env.bat
@@ -1,6 +1,6 @@
 @ECHO off
 
 SET NOAHBOT_HOME="C:\Users\Main\prog\NoahBot"
-SET NOAHBOT_DOTNET="C:\Program Files (x86)\MSBuild\14.0\Bin"
+SET NOAHBOT_DOTNET="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\Roslyn"
 
 CD /d %NOAHBOT_HOME%
