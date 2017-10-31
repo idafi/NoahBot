27a8a74cd11859a7bc59e8182566408976e10a59
idafi <none>
2017-10-31 16:15:59 -0500

    make entry point class internal

--- src/NoahBot/NoahBot.cs
+++ src/NoahBot/NoahBot.cs
@@ -1,12 +1,11 @@
 using System;
 using System.IO;
-using System.Threading.Tasks;
 using DSharpPlus;
 using Newtonsoft.Json;
 
 namespace NoahBot
 {
-	public static class NoahBot
+	internal static class NoahBot
 	{
 		static string[] proverbs;
 		
